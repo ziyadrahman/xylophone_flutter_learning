5654b6663d3f788bc800be3cf3895ac27f1fe4de
ziyadrahman <ziyad.rahman@icloud.com>
2024-07-10 10:59:58 +0530

    Added 7 audio notes successfully woking

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() => runApp(const XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
-  Expanded buildColorButton({required Color color}) {
+  Expanded buildColorButton({required Color color, required int trackNumber}) {
     return Expanded(
       child: TextButton(
         style: TextButton.styleFrom(
@@ -17,7 +17,7 @@ class XylophoneApp extends StatelessWidget {
             )),
         onPressed: () async {
           final player = AudioPlayer();
-          await player.play(AssetSource('note1.wav'));
+          await player.play(AssetSource('note$trackNumber.wav'));
         },
         child: const Text(" "),
       ),
@@ -40,10 +40,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildColorButton(color: Colors.red),
-              buildColorButton(color: Colors.green),
-              buildColorButton(color: Colors.yellow),
-              buildColorButton(color: Colors.purple),
+              buildColorButton(color: Colors.red, trackNumber: 1),
+              buildColorButton(color: Colors.green, trackNumber: 2),
+              buildColorButton(color: Colors.yellow, trackNumber: 3),
+              buildColorButton(color: Colors.purple, trackNumber: 4),
+              buildColorButton(color: Colors.blue, trackNumber: 5),
+              buildColorButton(color: Colors.pink, trackNumber: 6),
+              buildColorButton(color: Colors.pinkAccent, trackNumber: 7),
             ],
           ),
         ),
